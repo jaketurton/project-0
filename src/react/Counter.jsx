0cc1a0a9fb188fef4b52b1d94093d25113abaa65
Jake Turton <unpkjake@gmail.com>
2017-08-15 18:09:45 +0100

    changed counter functional component to a class

--- src/react/Counter.jsx
+++ src/react/Counter.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
-export default function Counter(props) {
+export default class Counter extends React.Component {
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {this.props.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>
+        )
+    }
+};
 
-  return (
-    <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {props.score} </div>
-        <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
+// Functional component (sans-state)
+
+// export default function Counter(props) {
+
+//   return (
+//     <div className="counter">
+//         <button className="counter-action decrement"> - </button>
+//         <div className="counter-score"> {props.score} </div>
+//         <button className="counter-action increment"> + </button>
+//     </div>
+//   );
+// }
